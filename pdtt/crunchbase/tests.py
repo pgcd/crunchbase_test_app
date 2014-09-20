7b02186481093164c86f065f435c8fab696bef95
Paolo Dente <pgcd93@gmail.com>
2014-09-20 17:26:34 +0200

    Adds first test (failing)

--- pdtt/crunchbase/tests.py
+++ pdtt/crunchbase/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class ApiAccessTest(TestCase):
+    def test_a_user_can_search_crunchbase(self):
+        response = self.client.get('/search')
+        self.assertEqual(response.status_code, 200)
