404043c1db75d44ef0d5f3b1c1c2b34c5598eaf8
Paolo Dente <pgcd93@gmail.com>
2014-09-21 10:31:16 +0200

    Refactors tests (failing), adds CrunchbaseQuery object to wrap API requests

--- pdtt/crunchbase/tests.py
+++ pdtt/crunchbase/tests.py
@@ -1,8 +1,10 @@
 from django.core import urlresolvers
 from django.test import TestCase
+from unittest import skip
+from crunchbase.views import CrunchbaseQuery
 
 
-class ApiAccessTest(TestCase):
+class FrontendAccessTest(TestCase):
     def test_a_user_can_search_crunchbase(self):
         response = self.client.get(urlresolvers.reverse('crunchbase:search'))
         self.assertEqual(response.status_code, 200)
@@ -11,3 +13,32 @@ class ApiAccessTest(TestCase):
         self.assertContains(response, '<a href="%s">Companies</a>' % companies_search_url)
         products_search_url = urlresolvers.reverse('crunchbase:search', args=('products',))
         self.assertContains(response, '<a href="%s">Products</a>' % products_search_url)
+
+    @skip("Not currently enabled")
+    def test_the_main_search_page_shows_first_ten_results_of_both(self):
+        response = self.client.get(urlresolvers.reverse('crunchbase:search'))
+        self.assertIn('companies_search_results', response.context)
+        self.assertEqual(len(response.context['companies_search_results']), 10)
+
+
+class ApiQueryTest(TestCase):
+    def setUp(self):
+        self.bcq = CrunchbaseQuery()
+        super(ApiQueryTest, self).setUp()
+
+    def test_crunchquery_can_list_endpoints(self):
+        endpoints = self.bcq.list_endpoints()
+        # I'd like the endpoints to be returned in a dict format: {verbose_name: uri}
+        self.assertIsInstance(endpoints, dict)
+        # For this project, we only need the Companies and the Products endpoints
+        self.assertIn('companies', endpoints)
+        self.assertIn('products', endpoints)
+
+    def test_all_endpoints_queries_are_aliased_as_attributes(self):
+        # In real life, this might possibly lead to shadowing but that's not a concern in our scenario, since we're only
+        # going to have companies and products
+        self.assertTrue(all([hasattr(self.bcq, x) for x in self.bcq.list_endpoints()]))
+
+    def test_crunchquery_can_connect_to_endpoints(self):
+        # a true API would require several verbs; initially, though, we need to be able to list stuff.
+        response = self.bcq.companies.list()
--- pdtt/crunchbase/views.py
+++ pdtt/crunchbase/views.py
@@ -1,8 +1,27 @@
 from django.views.generic import ListView
 
 
+
+
 class CrunchbaseSearchView(ListView):
     template_name = 'crunchbase/search_results.html'
-    
+
     def get_queryset(self):
         return []
+
+    def get_context_data(self, **kwargs):
+        data = super(CrunchbaseSearchView, self).get_context_data(**kwargs)
+        data['companies_search_results'] = None
+        return data
+
+
+class CrunchbaseQuery(object):
+    ENDPOINTS = {'companies': '', 'products': ''}
+
+    def list_endpoints(self):
+        return self.ENDPOINTS
+
+    def __getattr__(self, item):
+        if item in self.ENDPOINTS:
+            return self.ENDPOINTS[item]
+        raise AttributeError
